bda67e9a6e62209eae78a59108c5c4db271ff152
Miguel Ángel Sánchez Mares <mares112358@gmail.com>
2020-09-24 22:14:02 -0500

    Delete BasicGraph.py

--- Algoritmos de Ordenamiento/BasicGraph.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-
-x = np.random.uniform(0, 1)
-y = np.random.uniform(0, 1)
-print(x)
-print(y)
-plt.plot(x, y, color = 'red')
-plt.show()           # Display the plot
