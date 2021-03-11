db9435d6bab8a772aca2a6ce04201644ec62e06a
Miguel Mares <mares112358@gmail.com>
2021-03-10 22:00:33 -0600

    Completando implementacion del algoritmo

--- Backtracking/Maze_Problem.py
+++ Backtracking/Maze_Problem.py
@@ -15,11 +15,39 @@ class MazeProblem():
         if self.is_finished(x, y):
             return True
 
+        if self.is_valid(x, y):
+            self.solution_table[x][y] = 1
+
+            if self.solve(x + 1, y):
+                return True
+            if self.solve(x, y + 1):
+                return True
+            self.solution_table[x][y] = 0
+        return False
+
     def is_finished(self, x, y):
         if x == self.size - 1 and y == self.size - 1:
             self.solution_table[x][y] = 1
             return True
         return False
+    
+    def is_valid(self, x, y):
+        if x < 0 or x >= self.size:
+            return False
+        if y < 0 or y >= self.size:
+            return False
+        if self.maze[x][y] == 1:
+            return False
+        return True        
+
+    def show_solution(self):
+        for i in range(self.size):
+            for j in range(self.size):
+                if self.solution_table[i][j] == 1:
+                    print('S', end = ' ')
+                else:
+                    print('-', end = ' ')
+            print('\n')
 
 if __name__ == '__main__':
     m = [[0, 0, 1, 1],
@@ -28,4 +56,4 @@ if __name__ == '__main__':
          [1, 1, 0, 0]]
 
     maze = MazeProblem(m)
-    print(maze.solution_table)
+    maze.solve_maze()
